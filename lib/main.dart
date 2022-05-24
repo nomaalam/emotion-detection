ca6460961504803a37c3019f9aedd1bfac366838
nomaalam <nomaa848@gmail.com>
2022-05-25 02:07:23 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+import 'home.dart';
+
+List<CameraDescription>? cameras;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(new myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.orange),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
